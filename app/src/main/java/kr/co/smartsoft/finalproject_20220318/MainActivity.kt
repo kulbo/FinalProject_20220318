ed4f18737028f664646aa18def85f1dc3a44c9cc
이 성 노 <kulbo0926@gmail.com>
2022-03-18 11:39:49 +0900

    #1 binding 을 만들고 BaseActivity 생성했다

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/MainActivity.kt
@@ -2,10 +2,23 @@ package kr.co.smartsoft.finalproject_20220318
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.finalproject_20220318.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
