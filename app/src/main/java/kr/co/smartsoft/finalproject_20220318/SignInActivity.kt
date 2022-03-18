675d52bbc863889b12fd3884250a0c40a59dce4f
이 성 노 <kulbo0926@gmail.com>
2022-03-18 12:15:07 +0900

    #1 로그인 화면을 구성했다

---
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/SignInActivity.kt
@@ -0,0 +1,26 @@
+package kr.co.smartsoft.finalproject_20220318
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.finalproject_20220318.databinding.ActivitySignInBinding
+
+class SignInActivity : BaseActivity() {
+    lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in)
+
+        setUpEvents()
+        setValues()
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
