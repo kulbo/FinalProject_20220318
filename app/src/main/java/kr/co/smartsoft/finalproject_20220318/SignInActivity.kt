3c3981c6e7af7dfe51d45dd2df30beec164c7ca8
이 성 노 <kulbo0926@gmail.com>
2022-03-18 12:56:40 +0900

    #1 아이디와 비밀번호를 입력하지 않으면 입력해 달라고   했다.

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/SignInActivity.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/SignInActivity.kt
@@ -2,6 +2,7 @@ package kr.co.smartsoft.finalproject_20220318
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.finalproject_20220318.databinding.ActivitySignInBinding
 
@@ -18,6 +19,20 @@ class SignInActivity : BaseActivity() {
 
     override fun setUpEvents() {
 
+        binding.btnLogIn.setOnClickListener {
+            val edtId = binding.edtEmail.text.toString()
+            val edtPasswd = binding.edtPassword.text.toString()
+
+            if (edtId == ""){
+                Toast.makeText(mContext, "이메일이 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (edtPasswd == ""){
+                Toast.makeText(mContext, "비밀번호가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
 
     override fun setValues() {
