37a944665696fdb00d8b1b2fd5c5185e313dd393
kulbo <kulbo@hanmail.net>
2022-03-20 14:50:14 +0900

    #7 프로파일화면 그리기  - binding Fragment를 수정함.

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/MyProfileFragment.kt
@@ -6,11 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.finalproject_20220318.R
-import kr.co.smartsoft.finalproject_20220318.databinding.FragmentAppointmentListBinding
+import kr.co.smartsoft.finalproject_20220318.databinding.FragmentMyProfileBinding
 
 class MyProfileFragment : BaseFragment() {
 
-    lateinit var binding : FragmentAppointmentListBinding
+    lateinit var binding : FragmentMyProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
