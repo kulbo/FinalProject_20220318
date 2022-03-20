cf4501a48240e26097fa6612a7c91d5ae61c4f55
kulbo <kulbo@hanmail.net>
2022-03-20 11:19:36 +0900

    #6 약속목록화면 그리기  - AppointmentListFragment.kt 의 기본 로직 구성

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/AppointmentListFragment.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/AppointmentListFragment.kt
@@ -1,4 +1,36 @@
 package kr.co.smartsoft.finalproject_20220318.fragments
 
-class AppointmentListFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.finalproject_20220318.R
+import kr.co.smartsoft.finalproject_20220318.databinding.FragmentAppointmentListBinding
+
+class AppointmentListFragment : BaseFragment() {
+
+    lateinit var binding : FragmentAppointmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_appointment_list, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setUpEvents()
+        setValues()
+    }
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
 }
