943f94266770fa9bb6f4f9560945dcd94dfb43f0
kulbo <kulbo@hanmail.net>
2022-03-20 11:43:03 +0900

    #7 프로파일화면 그리기  - MyProfileFragment 생성  - fragment_my_profile 과 연결시킴

---
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/MyProfileFragment.kt
@@ -0,0 +1,36 @@
+package kr.co.smartsoft.finalproject_20220318.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.finalproject_20220318.R
+import kr.co.smartsoft.finalproject_20220318.databinding.FragmentAppointmentListBinding
+
+class MyProfileFragment : BaseFragment() {
+
+    lateinit var binding : FragmentAppointmentListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_profile, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setUpEvents()
+        setValues()
+    }
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
