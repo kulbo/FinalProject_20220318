abe7ec5392a068797844f7c1e36b1d465f8fa360
kulbo <kulbo@hanmail.net>
2022-03-20 20:28:00 +0900

    #7 프로파일화면 그리기  - 로그아웃버튼을 누르면 이벤트처리가 되도록 프로그램 작성

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/fragments/MyProfileFragment.kt
@@ -1,12 +1,17 @@
 package kr.co.smartsoft.finalproject_20220318.fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.finalproject_20220318.R
+import kr.co.smartsoft.finalproject_20220318.SplashActivity
 import kr.co.smartsoft.finalproject_20220318.databinding.FragmentMyProfileBinding
+import kr.co.smartsoft.finalproject_20220318.utils.ContextUtil
 
 class MyProfileFragment : BaseFragment() {
 
@@ -27,7 +32,19 @@ class MyProfileFragment : BaseFragment() {
         setValues()
     }
     override fun setUpEvents() {
-
+        binding.btnLogOut.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+                .setTitle("로그아웃")
+                .setMessage("정말 로구아웃")
+                .setPositiveButton("확인",DialogInterface.OnClickListener { dialogInterface, i ->
+                    ContextUtil.setLoginUserToken(mContext, "")
+                    val myIntent = Intent(mContext, SplashActivity::class.java)
+                    myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(myIntent)
+                })
+                .setNegativeButton("최소", null)
+                .show()
+        }
     }
 
     override fun setValues() {
