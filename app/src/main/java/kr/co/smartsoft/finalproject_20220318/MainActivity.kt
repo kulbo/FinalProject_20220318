32111ed1c1a0f5d7e748bfee65bcb6fc33ded7c8
kulbo <kulbo@hanmail.net>
2022-03-20 14:36:51 +0900

    #5 메인화면 만들기  - 화면 맨 아래에 메뉴화면을 만들었다.  - 메뉴 화면을 클릭하면 두개의 fragment 화면이 보이도록 만들었다.

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/MainActivity.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.smartsoft.finalproject_20220318
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
 import kr.co.smartsoft.finalproject_20220318.adapters.MainViewPager2Adapter
 import kr.co.smartsoft.finalproject_20220318.databinding.ActivityMainBinding
 
@@ -13,9 +14,29 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        setUpEvents()
+        setValues()
     }
 
     override fun setUpEvents() {
+        binding.btnBottomNav.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.myAppointment -> binding.mainViewPager2.currentItem = 0
+                R.id.myProfile -> binding.mainViewPager2.currentItem = 1
+            }
+            return@setOnItemSelectedListener true
+        }
+        binding.mainViewPager2.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                binding.btnBottomNav.selectedItemId = when(position) {
+                    0 -> R.id.myAppointment
+                    else -> R.id.myProfile
+                }
+            }
+        })
 
     }
 
