adf03abc1a259b931e2e1d0cfb438d2163d744b8
kulbo <kulbo@hanmail.net>
2022-03-21 19:42:34 +0900

    #8 약속등록화면 그리기  - EditAppointmentActivity 생성  - 기초 설정

---
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/EditAppointmentActivity.kt
@@ -0,0 +1,25 @@
+package kr.co.smartsoft.finalproject_20220318
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import kr.co.smartsoft.finalproject_20220318.databinding.ActivityEditAppointmentBinding
+
+class EditAppointmentActivity : BaseActivity() {
+    lateinit var binding: ActivityEditAppointmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_appointment)
+        setUpEvents()
+        setValues()
+    }
+
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
