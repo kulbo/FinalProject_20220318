1fc7fe6de77e4032beaba1196f3333c9f96dce8e
kulbo <kulbo@hanmail.net>
2022-03-22 12:21:24 +0900

    #8 약속등록화면 그리기  - 시계를 보이게 하여 일자를 선택할 수 있게 하였다.

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/EditAppointmentActivity.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/EditAppointmentActivity.kt
@@ -1,12 +1,18 @@
 package kr.co.smartsoft.finalproject_20220318
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.databinding.DataBindingUtil
 import kr.co.smartsoft.finalproject_20220318.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
     lateinit var binding: ActivityEditAppointmentBinding
+
+    val mSelectedDateTime = Calendar.getInstance() // 기본값 : 현재일시를 가져온다.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +23,27 @@ class EditAppointmentActivity : BaseActivity() {
 
     override fun setUpEvents() {
 
+        binding.txtAppointmentDate.setOnClickListener {
+            val dsl = object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+                    mSelectedDateTime.set(year, month, day)
+                    val sdf = SimpleDateFormat("yy/MM/dd")
+                    binding.txtAppointmentDate.text = sdf.format(mSelectedDateTime.time)
+                }
+            }
+
+            val dpd = DatePickerDialog(
+                mContext,
+                dsl,
+                mSelectedDateTime.get(Calendar.YEAR),
+                mSelectedDateTime.get(Calendar.MONTH),
+                mSelectedDateTime.get(Calendar.HOUR_OF_DAY)
+            ).show()
+       }
+
+        binding.txtAppointmentTime.setOnClickListener {
+
+        }
     }
 
     override fun setValues() {
