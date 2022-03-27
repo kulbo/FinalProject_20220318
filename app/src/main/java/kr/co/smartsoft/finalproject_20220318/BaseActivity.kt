f50def45dbc644cff7f4fba6df05611509025cd5
kulbo <kulbo@hanmail.net>
2022-03-27 14:28:52 +0900

    #15 액션바 만들기  - 액션바 xml 을 만들었다.  - BaseActivity.kt에 액션바가 보이도록 구성

--- app/src/main/java/kr/co/smartsoft/finalproject_20220318/BaseActivity.kt
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/BaseActivity.kt
@@ -2,7 +2,11 @@ package kr.co.smartsoft.finalproject_20220318
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import kr.co.smartsoft.finalproject_20220318.api.APIList
 import kr.co.smartsoft.finalproject_20220318.api.ServerApi
 
@@ -11,6 +15,9 @@ abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var apiList : APIList
 
+    lateinit var txtTitle: TextView
+    lateinit var imgAdd: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
@@ -18,8 +25,24 @@ abstract class BaseActivity : AppCompatActivity() {
         val retrofit = ServerApi.getRerofit(mContext)
         apiList = retrofit.create(APIList::class.java)
 
+        supportActionBar?.let {
+            setActionBar()
+        }
+
     }
 
     abstract fun setUpEvents()
     abstract fun setValues()
+
+    fun setActionBar() {
+        val defaultActionBar = supportActionBar!!
+
+        defaultActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defaultActionBar.setCustomView(R.layout.my_action_bar)
+        val toolBar = defaultActionBar.customView.parent as Toolbar
+        toolBar.setContentInsetsAbsolute(0, 0)
+
+        txtTitle = defaultActionBar.customView.findViewById(R.id.txtTitle)
+        imgAdd = defaultActionBar.customView.findViewById(R.id.imgPlus)
+    }
 }
