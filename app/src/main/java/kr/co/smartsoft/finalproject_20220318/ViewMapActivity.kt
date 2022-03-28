409e872582969395910fb37fd559384ab0131a00
kulbo <kulbo@hanmail.net>
2022-03-28 11:25:59 +0900

    #17 약속장소지도 화면 그리기  - 약속장소 지도화면을 생성하고 기본 설정작업

---
+++ app/src/main/java/kr/co/smartsoft/finalproject_20220318/ViewMapActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.smartsoft.finalproject_20220318
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_map)
+    }
+}
